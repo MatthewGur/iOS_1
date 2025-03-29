21fef1119a7d7f0769e339dc1f448d23a2344a9f
MatthewGur <60626553+MatthewGur@users.noreply.github.com>
2025-03-29 19:26:27 +0300

    Create about_me.swift

---
+++ about_me.swift
@@ -0,0 +1,6 @@
+let firstName: String = "Матвей"
+let lastName: String = "Гуреев"
+let groupNumber: Int = 1371
+let specialty: String = "ИСиТ в бизнесе"
+
+print("Краткая информация: \(firstName) \(lastName), группа \(groupNumber), специальность - \(specialty)")
